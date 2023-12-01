00882bcc70bc3eb489da5757c07d614ad84af87a
maxijayme <maxijayme@gmail.com>
2023-12-01 20:29:47 +0100

    posts section in dashboard

--- platzi_course/routes/web.php
+++ platzi_course/routes/web.php
@@ -1,19 +1,39 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
 
-/**Route::get('/', [PageController::class,('home')])->name('home');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::get('/blog', [PageController::class,('blog')])->name('blog');
+Route::controller(PageController::class)->group(function () {
 
-Route::get('/blog/{slug}', [PageController::class,('post')])->name('post');**/
-
-Route::controller(PageController::class)->group(function (){
-    Route::get('/', ('home'))->name('home');
-
-    Route::get('/blog', ('blog'))->name('blog');
-
-    Route::get('/blog/{post:slug}', ('post'))->name('post');
+    Route::get('/',           'home')->name('home');
+    Route::get('blog',        'blog')->name('blog');
+    Route::get('blog/{post:slug}', 'post')->name('post');
 
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('posts', PostController::class)->except(['show']);
+});
+
+
+require __DIR__.'/auth.php';
